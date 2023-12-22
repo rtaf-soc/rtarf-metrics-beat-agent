355ea16661c459e15014acd416137fd7b20dc46e
pjames <pjames@gmail.com>
2023-12-22 08:48:24 +0700

    Fixed env substitution

--- start-agent.bash
+++ start-agent.bash
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-cat << EOF > .env
+cat << EOF >> .env
 HOMEDIR=${HOME}
 EOF
+
+CFG_FILE=configs/metricbeat.yml
+
+sudo chown root:root ${CFG_FILE}
+sudo chmod 600 ${CFG_FILE}
 
 sudo docker-compose down
 sudo docker-compose up -d
