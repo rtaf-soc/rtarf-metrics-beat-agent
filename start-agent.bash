90dba24977ac59c53632f74813c95f8ff3326056
pjames <pjames@gmail.com>
2023-12-26 08:50:38 +0700

    Added heartbeat

--- start-agent.bash
+++ start-agent.bash
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-TPL_FILE=configs/metricbeat-template.yml
-CFG_FILE=configs/metricbeat.yml
+MB_TPL_FILE=configs/metricbeat-template.yml
+MB_CFG_FILE=configs/metricbeat.yml
 
-sudo cp ${TPL_FILE} ${CFG_FILE}
-sudo chown root:root ${CFG_FILE}
-sudo chmod 600 ${CFG_FILE}
+HB_TPL_FILE=configs/heartbeat-template.yml
+HB_CFG_FILE=configs/heartbeat.yml
+
+sudo cp ${MB_TPL_FILE} ${MB_CFG_FILE}
+sudo chown root:root ${MB_CFG_FILE}
+sudo chmod 600 ${MB_CFG_FILE}
+
+sudo cp ${HB_TPL_FILE} ${HB_CFG_FILE}
+sudo chown root:root ${HB_CFG_FILE}
+sudo chmod 600 ${HB_CFG_FILE}
 
 sudo docker-compose down
 sudo docker-compose up -d
